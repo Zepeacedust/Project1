a668c922ddcaa930cef0054ba9548155ca5248a6
Zepeacedust <arnor0903@gmail.com>
2022-10-09 19:16:55 +0000

    laga smá jank til að láta aallt kefrið virka

--- Code/structured_logging/logger_creation/logger_factory.py
+++ Code/structured_logging/logger_creation/logger_factory.py
@@ -5,5 +5,10 @@ from Code.structured_logging.logger.logger import Logger
 
 def create_logger(logger_config: LoggerConfig) -> Logger:
     container = Container()
-    container.config.from_value(logger_config)
+    # smá jank en að nota from_value virkar ekki, síðan þarf að passa alvöru config dict áfram svo það er sent með.
+    # frekar jank aftur en það virkar og er ekki allt of heimskt. 
+    config_dict = logger_config.dict()
+    config_dict["config_object"] = logger_config
+    
+    container.config.from_dict(config_dict)
     return container.logger()
