33d526619035b5d94a2fd8176068604671116b62
Huginn-Muninn <tomas.orri.arnarsson@gmail.com>
2022-10-09 15:51:56 +0000

    ekkert gafulegt

--- Code/structured_logging/command_queue/queue.py
+++ Code/structured_logging/command_queue/queue.py
@@ -5,7 +5,6 @@ import threading
 class Queue:
     # TODO: we also need to inject the async delay time into the constructor
     def __init__(self):
-        raise NotImplementedError()
         self.__thread = threading.Thread(target=self.__process)
         self.__thread.daemon = True
         self.__thread.start()
