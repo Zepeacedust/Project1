4df30008b2f29ab2a807469db7ce0ceb77085d20
Zepeacedust <arnor0903@gmail.com>
2022-10-09 13:56:53 +0000

    clean up console_sink

--- Code/structured_logging/sinks/console_sink.py
+++ Code/structured_logging/sinks/console_sink.py
@@ -1,6 +1,7 @@
-from json import dumps, json
+import json
+from Code.structured_logging.sinks.I_sink import ISink
 
-class ConsoleSink():
-    def print_json(self, dictionary):
+class ConsoleSink(ISink):
+    def sink_data(self, dictionary):
         dict_to_json = json.dumps(dictionary)
         print(dict_to_json)
