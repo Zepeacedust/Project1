f027102c903daba1ab0af61d51bf224b991565e6
Zepeacedust <arnor0903@gmail.com>
2022-10-09 16:21:36 +0000

    Implement Logger

--- Code/structured_logging/logger/logger.py
+++ Code/structured_logging/logger/logger.py
@@ -1,7 +1,7 @@
 from typing import Any, Iterable
 from structured_logging.command_queue.queue import Queue
 from structured_logging.configuration.logger_config import LoggerConfig
-
+from Code.structured_logging.logger.logging_command import LoggingCommand
 
 class Logger:
     def __init__(self, logger_config: LoggerConfig, logging_queue: Queue):
@@ -9,4 +9,10 @@ class Logger:
         self.__logging_queue = logging_queue
 
     def log(self, **kwargs: Iterable[Any]):
-        raise NotImplementedError()
+        data = kwargs.copy()
+        self.__logger_config.processor(data)
+        command = LoggingCommand(data)
+        if self.__logger_config.is_async:
+            self.__logging_queue.add(command)
+        else:
+            command.execute()
