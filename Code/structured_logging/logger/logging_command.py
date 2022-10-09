4e724d01fed9923c9d9f76a0f11241fd13d4e159
Huginn-Muninn <tomas.orri.arnarsson@gmail.com>
2022-10-09 16:20:05 +0000

    logging_command

--- Code/structured_logging/logger/logging_command.py
+++ Code/structured_logging/logger/logging_command.py
@@ -1,4 +1,9 @@
-from logger import Logger
+from Code.structured_logging.command_queue.command import Command
+from Code.structured_logging.sinks.I_sink import ISink
 
-class LoggingCommand():
-    
+class LoggingCommand(Command):
+    def __init__(self, data, sink:ISink):
+        self.data = data
+        self.sink = sink
+    def execute(self):
+        self.sink.sink_data(self.data)
