c3bab40ade170a9321bc7974b9d4c07f34c6f3b6
Zepeacedust <arnor0903@gmail.com>
2022-10-09 14:21:53 +0000

    fix abstract processor

--- Code/structured_logging/processors/abstract_processor.py
+++ Code/structured_logging/processors/abstract_processor.py
@@ -1,6 +1,7 @@
 from Code.structured_logging.processors.I_processor import IProcessor
 from abc import abstractmethod
 class AbstractProcessor(IProcessor):
+    _next_processor = None
     def set_next(self, processor: IProcessor) -> IProcessor:
         self._next_processor = processor
         return processor
